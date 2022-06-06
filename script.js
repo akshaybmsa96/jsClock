252b46268becbfa62eca37bfc292ee843b733ec9
Akshay <akshaybmsa96@gmail.com>
2022-06-07 00:32:06 +0530

    minute and hour hand movement

--- script.js
+++ script.js
@@ -10,18 +10,17 @@ const handHour = document.getElementById("handHour");
 const setHands = () => {
   timeContainer.innerHTML = date.toLocaleTimeString("en-In");
   let sec = (360 / 60) * date.getSeconds();
-  let min = (360 / 60) * date.getMinutes();
-  let hour = (360 / 12) * date.getHours();
+  let min = (360 / 60) * (date.getMinutes() + date.getSeconds() / 60);
+  let hour = (360 / 12) * (date.getHours() + date.getMinutes() / 60);
   console.log(hour, date.getHours());
-  handHour.style.transform = `rotate(${hour}deg)`;
-  handMinute.style.transform = `rotate(${min}deg)`;
-  handSecond.style.transform = `rotate(${sec}deg)`;
+  handHour.style.transform = `rotate(${hour % 360}deg)`;
+  handMinute.style.transform = `rotate(${min % 360}deg)`;
+  handSecond.style.transform = `rotate(${sec % 360}deg)`;
 };
 
 setHands();
 
 setInterval(() => {
   date = new Date();
-
   setHands();
 }, 1000);
